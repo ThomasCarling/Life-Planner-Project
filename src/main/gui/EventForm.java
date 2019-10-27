2454d06dc96506042fc6826baa6f76da8d16bda5
Thomas Carling <jtmcarling@yahoo.co.uk>
2019-10-27 22:30:51 +0000

    add EventForm

---
+++ src/main/gui/EventForm.java
@@ -0,0 +1,102 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+public class EventForm extends JPanel{
+    
+    private static final long serialVersionUID = 6036286048012599252L;
+    private JLabel nameLabel;
+    private JLabel lengthLabel;
+    private JLabel locationLabel;
+    private JTextField nameField;
+    private JTextField lengthField;
+    private JTextField locationField;
+    private JButton enterButton;
+    
+    public EventForm() {
+	Dimension dim = getPreferredSize();
+	dim.width = 250;
+	setPreferredSize(dim);
+	setLayout(new GridBagLayout());
+	
+	nameLabel = new JLabel("Event name :  ");
+	lengthLabel = new JLabel("Length (in minutes) :  ");
+	locationLabel = new JLabel("Location (postcode) :  ");
+	nameField = new JTextField(10);
+	lengthField = new JTextField(10);
+	locationField = new JTextField(10);
+	enterButton = new JButton("Enter");
+	
+	Border innerBorder = BorderFactory.createTitledBorder(BorderFactory.createLoweredBevelBorder(), "Add new event");
+	Border outerBorder = BorderFactory.createEtchedBorder();
+	setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+	
+	GridBagConstraints gbc = new GridBagConstraints();
+	gbc.fill = GridBagConstraints.NONE;
+	
+	//////first row//////
+	gbc.anchor = GridBagConstraints.LINE_END;
+	gbc.weightx = 1;
+	gbc.weighty = 1;
+	gbc.gridx = 0;
+	gbc.gridy = 0;
+	add(nameLabel, gbc);
+	
+	gbc.anchor = GridBagConstraints.LINE_START;
+	gbc.weightx = 1;
+	gbc.weighty = 1;
+	gbc.gridx = 1;
+	gbc.gridy = 0;
+	add(nameField, gbc);
+	
+	//////second row//////
+	gbc.anchor = GridBagConstraints.LINE_END;
+	gbc.weightx = 1;
+	gbc.weighty = 1;
+	gbc.gridx = 0;
+	gbc.gridy = 1;
+	add(lengthLabel, gbc);
+	
+	gbc.anchor = GridBagConstraints.LINE_START;
+	gbc.weightx = 1;
+	gbc.weighty = 1;
+	gbc.gridx = 1;
+	gbc.gridy = 1;
+	add(lengthField, gbc);
+	
+	//////third row//////
+	gbc.anchor = GridBagConstraints.LINE_END;
+	gbc.weightx = 1;
+	gbc.weighty = 1;
+	gbc.gridx = 0;
+	gbc.gridy = 2;
+	add(locationLabel, gbc);
+	
+	gbc.anchor = GridBagConstraints.LINE_START;
+	gbc.weightx = 1;
+	gbc.weighty = 1;
+	gbc.gridx = 1;
+	gbc.gridy = 2;
+	add(locationField, gbc);
+	
+	//////last row//////
+	gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+	gbc.weightx = 1;
+	gbc.weighty = 30;
+	gbc.gridx = 1;
+	gbc.gridy = 3;
+	gbc.insets = new Insets(5, 0, 0, 0);
+	add(enterButton, gbc);
+    }
+    
+}
--- src/main/gui/MainFrame.java
+++ src/main/gui/MainFrame.java
@@ -5,21 +5,36 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+/**
+ * JFrame for my project, also currently contains main 
+ * method to run the project.
+ */
 public class MainFrame extends JFrame {
+    
+    private static final long serialVersionUID = 1L;
+    private Menu menu;
+    private EventForm eventForm;
     
     public MainFrame(String title) {
 	super(title);
-	setLayout(new BorderLayout());
-	setJMenuBar(new Menu());
 	
+	setLayout(new BorderLayout());
+	
+	menu = new Menu();
+	eventForm = new EventForm();
+	
+	setJMenuBar(menu);
+	add(eventForm, BorderLayout.WEST);
+	
+	
+	setSize(500, 400);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setVisible(true);	
     }
     
     public static void main(String[] args) {
 	SwingUtilities.invokeLater( () -> {
-	    JFrame frame = new MainFrame("Life Planner");
-	    frame.setSize(500, 400);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setVisible(true);
+	    new MainFrame("Life Planner");
 	});
     }
 
